d3effadaee8614ce4af70b40fd87a40415036587
Maksim Baranov <oooinfo@mail.ru>
2023-05-23 16:02:56 +0300

    добавил секцию reviews

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,41 @@
 //= components/script.js
 //= components/script2.js
 //= components/script3.js
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js'
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  
+  
+  spaceBetween: 40,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    991.98: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+  },
+
+});
