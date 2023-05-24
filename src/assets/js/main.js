bd860166f276f5ed03db2719818a11cb04a42902
Maksim Baranov <oooinfo@mail.ru>
2023-05-24 12:31:13 +0300

    добавил модальные изображения

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,8 +1,6 @@
-//= components/script.js
-//= components/script2.js
-//= components/script3.js
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js'
+// ? ========= slider ============
 
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js'
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
@@ -39,3 +37,44 @@ const swiper = new Swiper('.swiper', {
   },
 
 });
+
+
+// ? ========= modal ============
+const accredImg = document.querySelector('#accredImg')
+const reviewsImages = document.querySelectorAll('#reviewImg')
+
+
+function createModal() {
+  const modal = document.createElement('div');
+  const close = document.createElement('span');
+  const image = document.createElement('img');
+
+  modal.classList.add('modal')
+  close.classList.add('close')
+  image.classList.add('modal-content')
+
+  modal.setAttribute('id', 'myModal')
+  image.setAttribute('id', 'img01')
+  image.src = this.src
+  
+
+  close.innerHTML = '&times;'
+
+  modal.append(close)
+  modal.append(image)
+  document.body.prepend(modal)
+
+
+  close.addEventListener('click', () => {
+    modal.remove()
+  })
+  modal.addEventListener('click', () => {
+    modal.remove()
+  })
+}
+
+accredImg.addEventListener('click', createModal)
+
+for (let reviewsImg of reviewsImages) {
+  reviewsImg.addEventListener('click', createModal)
+}
